fa0e952c8e1a3dbacd58b993a373545963df8f2b
Chanartip <Chanartip.Soonthornwan@gmail.com>
2018-05-03 12:33:38 -0700

    5/3/2018

--- src/MCU1_BT_on_Car/mcu1_bt_on_car.d
+++
@@ -1,5 +0,0 @@
-.\mcu1_bt_on_car.o: MCU1_BT_on_Car.c
-.\mcu1_bt_on_car.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
-.\mcu1_bt_on_car.o: ../lib/tm4c123gh6pm.h
-.\mcu1_bt_on_car.o: ../lib/PLL.h
-.\mcu1_bt_on_car.o: ../lib/UART.h
